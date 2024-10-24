d0e484ae8c2243fce2b9ba398af8a3034b896fed
UchihaWeb3 <guptayash1501@gmail.com>
2024-10-24 23:21:50 +0530

    wagmi created

---
+++ hardhat/hardhat.config.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.24",
+  networks: {
+    hardhat: {
+      forking: {
+        url: process.env.RPC_URL,
+      },
+      chainId: 59141,
+    }
+  }
+};
